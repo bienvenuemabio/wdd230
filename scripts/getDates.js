4384ea0a49bba72d09fb110685a1b57aecd0d32e
kouassi bienvenue adjoua felicite mabio - bienvenuemabio <kou22008@byui.edu>
2025-01-30 20:33:17 +0000

    make the correction for the year

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,5 +1,3 @@
-const currentyear = document.getElementById('#currentyear');
-const today = new Date();
-currentyear.textContent = today.getFullYear();
-const modifdat = document.getElementById('#dat');
-modifdat.textContent = today;
+
+const currentyear = document.getElementById("currentyear");
+currentyear.textContent = new Date().getFullYear();
